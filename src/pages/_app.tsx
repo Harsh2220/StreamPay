d8847e19ff75b7455d2be15dc9e9630aac99a26f
Harsh <harshgajjar145@gmail.com>
2023-12-29 21:14:48 +0530

    fea: basic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import SolanaProvider from "@/components/providers/solanaProvider";
+import { ThemeProvider } from "@/components/providers/themeProvider";
+import "@/styles/globals.css";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <SolanaProvider>
+        <Component {...pageProps} />
+      </SolanaProvider>
+    </ThemeProvider>
+  );
 }
